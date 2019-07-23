0aa38b5d51532ecd1b45f680103e950e754fc800
Hong-She Liang <starofrainnight@gmail.com>
2019-07-23 14:50:53 +0800

    Update rtpl-pypackage framework to 0.3.1

--- pypuck/__main__.py
+++ pypuck/__main__.py
@@ -4,13 +4,38 @@
 """Console script for pypuck."""
 
 import click
+import pycurl
+import os.path
+from attrdict import AttrDict
+
 
 @click.command()
-def main(args=None):
+def main(**kwargs):
     """Console script for pypuck."""
-    click.echo("Replace this message by putting your code into "
-               "pypuck.__main__.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+
+    kwargs = AttrDict(kwargs)
+
+    url = "https://github.com/winpython/winpython/releases/download/1.11.20181031/Winpython64-3.7.1.0Zero.exe"
+    proxy = "%s://%s:%s" % ("socks5", "127.0.0.1", "11086")
+    curl = pycurl.Curl()
+    try:
+        curl.setopt(pycurl.URL, url)
+        file = open(os.path.basename(url), "wb")
+        curl.setopt(pycurl.WRITEFUNCTION, file.write)
+        curl.setopt(pycurl.FOLLOWLOCATION, 1)
+        curl.setopt(pycurl.HEADER, True)
+        curl.setopt(pycurl.NOPROGRESS, 0)
+        curl.setopt(pycurl.SSL_VERIFYPEER, False)
+        curl.setopt(pycurl.SSL_VERIFYHOST, False)
+        curl.setopt(pycurl.VERBOSE, 1)
+        curl.setopt(pycurl.PROXY, proxy)
+        curl.setopt(
+            pycurl.USERAGENT,
+            "Mozilla/5.2 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50324)",
+        )
+        curl.perform()
+    finally:
+        curl.close()
 
 
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -3,56 +3,51 @@
 
 """The setup script."""
 
+import io
 from setuptools import setup, find_packages
 
-with open('README.rst', encoding='utf-8') as readme_file, \
-    open('HISTORY.rst', encoding='utf-8') as history_file:
-    long_description = (readme_file.read() + "\n\n" + history_file.read())
+with io.open("README.rst", encoding="utf-8") as readme_file, io.open(
+    "HISTORY.rst", encoding="utf-8"
+) as history_file:
+    long_description = readme_file.read() + "\n\n" + history_file.read()
 
-install_requires = [
-    'click>=6.0',
-    # TODO: put package requirements here
-]
+install_requires = ["click>=6.0", "attrdict", "pycurl"]
 
 setup_requires = [
-    'pytest-runner',
+    "pytest-runner",
     # TODO(starofrainnight): put setup requirements (distutils extensions, etc.) here
 ]
 
 tests_requires = [
-    'pytest',
+    "pytest",
     # TODO: put package test requirements here
 ]
 
 setup(
-    name='pypuck',
-    version='0.0.1',
+    name="pypuck",
+    version="0.0.1",
     description="A script for compile python program to portable executable",
     long_description=long_description,
     author="Hong-She Liang",
-    author_email='starofrainnight@gmail.com',
-    url='https://github.com/starofrainnight/pypuck',
+    author_email="starofrainnight@gmail.com",
+    url="https://github.com/starofrainnight/pypuck",
     packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            'pypuck=pypuck.__main__:main'
-        ]
-    },
+    entry_points={"console_scripts": ["pypuck=pypuck.__main__:main"]},
     include_package_data=True,
     install_requires=install_requires,
     license="Apache Software License",
     zip_safe=False,
-    keywords='pypuck',
+    keywords="pypuck",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
-    test_suite='tests',
+    test_suite="tests",
     tests_require=tests_requires,
     setup_requires=setup_requires,
 )
