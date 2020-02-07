d0288b3b0de0005fa8ad242d68efb6d4c6232e38
Hong-She Liang <starofrainnight@gmail.com>
2020-02-07 16:25:22 +0800

    Bump version: 0.0.1 → 0.0.2

--- pypuck/__init__.py
+++ pypuck/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """Hong-She Liang"""
 __email__ = "starofrainnight@gmail.com"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ tests_requires = [
 
 setup(
     name="pypuck",
-    version="0.0.1",
+    version="0.0.2",
     description="A script for compile python program to portable executable",
     long_description=long_description,
     author="Hong-She Liang",
